244b89699c95851bfe37dbe8f19ef4a8a4073cdd
JeronAlford44 <jalford3@lion.lmu.edu>
2023-04-03 15:35:15 -0700

    Basic MUI styling working now

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,23 @@ import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+import { makeStyles } from '@material-ui/core/styles'
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+const useStyles = makeStyles(
+  theme => ({
+    root: {
+      fontWeight: 'bold',
+      padding: theme.spacing(1),
+      margin: theme.spacing(1),
+    },
+  }),
+  { name: 'App' }
+)
+
+const App = (props: object) => {
+  const classes = useStyles(props)
+
+  return <section className={classes.root}>HELLO</section>
 }
 
 export default App
